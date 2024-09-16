bc470fc7b493315dd065c9adbad0df13655fc6b9
JGK19 <106597685+JGK19@users.noreply.github.com>
2024-09-16 12:20:56 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+from simplex import *
+
+def main():
+    cost = np.array([-5, -2]) #coeficientes da função para ser maximizada //x0 e x1
+    matrix_R = np.array([[1,1],[1,0],[0,1]]) # coeficientes das variaveis x0 e x1 no sistema de restrições
+    b = np.array([4, 3, 2]) #lado direito das restrições
+
+    basic_vars, solution, nonbasic_vars = simplex(cost, matrix_R, b)
+    print("x:",basic_vars, " = ", solution)
+    print("x:",nonbasic_vars, " = ", 0)
+
+
+    return
+
+main()
